42aba8ef9361881f2dcb44ac0e52858f63d79c66
phamanhtu1196 <tupa@vinsofts.com>
2018-08-14 00:02:15 +0700

    update tối ưu html/css/js

---
+++ 05_tupa/demo-giaodien/templates/header.php
@@ -0,0 +1,35 @@
+<header>
+            <!-- header left -->
+            <div class="header-left">
+                <img src="images/logo.jpg" title="vinsofts" alt="vinsofts" width="94px" height="54px"><br/>
+                <p>Your success, our pride!</p>
+            </div>
+            <!-- end header left -->
+            <!-- header-right -->
+            <div class="header-right">
+                <div class="header-right-top">
+                    <ul>
+                        <li><img src="images/skype.jpg" alt="skype">Skype: Vinsofts.com</li>
+                        <li><img src="images/mail.jpg" alt="mail">Mail: Contact@vinsofts.com</li>
+                        <li><img src="images/phone.jpg" alt="phone">Hotline: 0988 988 988</li>
+                    </ul>
+                </div>
+                <nav class="menu">
+                    <ul class="menu-con">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Protfolio</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Recruitment</a></li>
+                    </ul>
+                    <ul class="icon">
+                            <li><a href="#"><img src="images/fb.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="images/tw.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="images/in.jpg" alt=""></a></li>
+                            <li><a href="#"><img src="images/g.jpg" alt=""></a></li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- end header-right -->
+        </header>

f37aa357614b244c4030b4bbf14d871ff4241efd
vinsofts-cuongnq <cuongnq@vinsofts.net>
2018-08-13 20:45:48 +0700

    sua psdtohtml

--- 02_cuongnq/Convert PSD to HTML/index.php
+++ 02_cuongnq/Convert PSD to HTML/index.php
@@ -11,6 +11,9 @@
 	<?php
 	echo file_get_contents('css/header.css');
 	?>
+		<?php
+	echo file_get_contents('css/footer.css');
+	?>
 </style>
 <link rel="stylesheet" href="css/main.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>

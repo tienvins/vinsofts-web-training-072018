0089e16db48618c4a033d79924dd9ce920c4a7ad
trongnd1996 <trongnd@vinsofts.net>
2018-08-21 10:10:52 +0700

    data

--- 06_trongnd/project/db/vinsofts.sql
+++ 06_trongnd/project/db/vinsofts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 20, 2018 at 11:41 PM
+-- Generation Time: Aug 21, 2018 at 10:10 AM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.2.9-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vinsofts-tupa`
+-- Database: `vinsofts`
 --
 
 -- --------------------------------------------------------
@@ -66,7 +66,10 @@ INSERT INTO `tbl_teams` (`pk_team_id`, `c_name`, `c_description`, `c_logo`, `lea
 (4, 'Phòng Kế Toán', 'Đây là phòng Kế Toán', '1534347815download.jpeg', '2'),
 (5, 'Phòng Giám Đốcaaaaa', 'Đây là phòng Giám Đốc', '1534347416like.png', '1'),
 (17, 'a', 'a', '1534413926a1.png', '3'),
-(18, 'aaabbb', 'bbbaaa', '1534413960opacity.png', '2');
+(18, 'aaabbb', 'bbbaaa', '1534413960opacity.png', '2'),
+(91, 'Bóng Đá', 'Sút', '15348161921534816192ca2.jpg', '8'),
+(92, 'thất sắc', 'abc', '1534816242betta2.jpeg', '18'),
+(93, 'hanoi1', '123', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -83,7 +86,7 @@ CREATE TABLE `tbl_user` (
   `c_remember_token` varchar(100) DEFAULT NULL,
   `c_img` varchar(255) DEFAULT NULL,
   `c_gender` tinyint(4) NOT NULL,
-  `c_date_of_birth` date NOT NULL,
+  `c_date_of_birth` date DEFAULT NULL,
   `c_identify_id` varchar(255) DEFAULT NULL,
   `c_phone_number` int(11) NOT NULL,
   `c_current_address` varchar(255) DEFAULT NULL,
@@ -105,15 +108,12 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`pk_user_id`, `c_name`, `c_email`, `c_email_personal`, `c_password`, `c_remember_token`, `c_img`, `c_gender`, `c_date_of_birth`, `c_identify_id`, `c_phone_number`, `c_current_address`, `c_permanent_address`, `c_graduate_from`, `c_salary`, `c_bank_account_number`, `c_hobby`, `c_family_description`, `c_language_skills`, `c_leave_days`, `role_id`, `team_id`, `c_status`) VALUES
-(1, 'Phạm Anh B', 'b@mail.com', 'b@mail.com', '123', '1', '1534385683s-l300.jpg', 1, '2018-08-11', '', 90, '', '', 1, 13000000, '', '', '', '', 1, 1, 1, 1),
-(2, 'Phạm Anh A', 'a@mail.com', 'a@mail.com', '202cb962ac59075b964b07152d234b70', '', '1534385705nancy18.jpg', 1, '2018-08-17', '', 12345678, '', '', 123456, 9000000, '', '', '', '', 3, 1, 1, 1),
-(3, 'Phạm Anh C', 'c@mail.com', 'c@mail.com', '123', '', '1534385761opacity.png', 3, '2018-07-30', '', 33333333, '', '', 3, 4500000, '', '', '', '', 3, 3, 3, 3),
-(8, 'Phạm Anh E', 'e@mail.com', 'e@mail.com', '202cb962ac59075b964b07152d234b70', '1', '1534413597download.jpeg', 1, '2018-08-17', '1', 111111, '1', '1', 11, 1111111111, '', '1', '1', '1', 11, 1, 1, 1),
-(18, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, NULL, 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
-(19, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, NULL, 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
-(20, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, NULL, 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
-(21, '', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, NULL, 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
-(22, 'w1w1', 'w@mail.com', 'w@mail.com', '202cb962ac59075b964b07152d234b70', '1', '15347828641534782864', 1, '2018-08-11', '1', 1, '1', '1', 1, 1, '', '1', '1', '1', 1, 1, 5, 1);
+(8, 'ADMIN', 'admin@mail.com', 'e@mail.com', '202cb962ac59075b964b07152d234b70', '1', '1534816151aaa.png', 1, '2018-08-17', '1', 111111, '1', '1', 11, 1111111111, '', '1', '1', '1', 11, 1, 1, 1),
+(18, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, '1534816311cakoi.jpg', 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
+(19, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, '1534816331ez.jpg', 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
+(20, 'aaaaaa', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, '1534816353pubg.jpeg', 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
+(21, '', 'asdasd@asd.com', 'asdasd@asd.com', '123', NULL, '1534816462betta.jpeg', 1, '2018-11-11', NULL, 1, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 1, 1, 1, 1),
+(22, 'w1w1', 'w@mail.com', 'w@mail.com', '202cb962ac59075b964b07152d234b70', '1', '1534816390pubg.jpeg', 1, '2018-08-11', '1', 1, '1', '1', 1, 1, '', '1', '1', '1', 1, 1, 5, 1);
 
 --
 -- Indexes for dumped tables
@@ -150,7 +150,7 @@ ALTER TABLE `tbl_leader`
 -- AUTO_INCREMENT for table `tbl_teams`
 --
 ALTER TABLE `tbl_teams`
-  MODIFY `pk_team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `pk_team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
